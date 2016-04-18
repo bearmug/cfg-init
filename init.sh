247e49680795caf4d386d0b4cb494d1b29c4d25e
bearmug <pavel.fadeev@gmail.com>
2016-04-18 21:17:48 +0000

    init script fixtures

--- init.sh
+++ init.sh
@@ -10,12 +10,9 @@ sudo apt-get update -y
 # install git, openjdk, gradle
 sudo apt-get -y install nginx git openjdk-8-jdk gradle-2.12
 
-# copy git and gradle configuration files to system locations
-cp -Rfu home/.g* ~/
-
 # link nginx web-root to /var/www Apache location and ~/Web alias
 cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
-sudo mkdir /var/www
+sudo mkdir /var/wwwt
 sudo chmod 777 /var/www
 ln -s /var/www ~/Web
 sudosed -i -- 's/\/usr\/share\/nginx\/html/\/var\/www/g' /etc/nginx/sites-available/default
@@ -23,3 +20,6 @@ sudosed -i -- 's/\/usr\/share\/nginx\/html/\/var\/www/g' /etc/nginx/sites-availa
 # clone any github repo rightaway
 git clone https://github.com/bearmug/config-common.git ~/config-common
 git clone https://github.com/bearmug/portfolio-bench.git ~/portfolio-bench
+
+# copy git and gradle configuration files to system locations
+cp -Rfu config-common/home/.g* ~/
