562b7a802fa595849c3b4d6e04a76c9ead0633be
bearmug <pavel.fadeev@gmail.com>
2016-04-28 01:45:12 +0300

    install xfce and Intellij IDEA

--- init.sh
+++ init.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
+# ========================================================================
+# initial system update
+# ========================================================================
 # setup custom PPA
 sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo add-apt-repository ppa:cwchien/gradle -y
+sudo add-apt-repository ppa:mmk2410/intellij-idea-community -y
 
 # update system
 sudo apt-get update -y
 
+
+# ========================================================================
+# install xfce4 desktop and Idea
+# ========================================================================
+sudo apt-get -y install -y ubuntu-desktop xfce4 xfce4-goodies xrdp intellij-idea-community
+sudo sed -i -- 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo passwd ubuntu
+
+# make xfce4 session default
+echo xfce4-session > ~/.xsession
+sudo cp /home/ubuntu/.xsession /etc/skel
+
+sudo service ssh restart
+sudo service xrdp restart
+
+# ========================================================================
+# toolchain setup
+# ========================================================================
 # install git, openjdk, gradle
 sudo apt-get -y install nginx git openjdk-8-jdk gradle-2.12
 
