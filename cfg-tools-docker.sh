25797af2cb1cdcb550f90e23bb730cea4ba58f01
bearmug <pavel.fadeev@gmail.com>
2019-01-22 01:42:35 +0300

    docker stuff complete

--- cfg-tools-docker.sh
+++ cfg-tools-docker.sh
@@ -1,35 +1,24 @@
 #!/bin/bash
 
 # ========================================================================
-# purge packages and prepare system
+# purge packages and install docker ce
 # ========================================================================
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
 sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg2 \
-    software-properties-common -y
-
-# ========================================================================
-# add Docker GPG key
-# ========================================================================
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-# ========================================================================
-# add stable Docker repository
-# ========================================================================
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-# ========================================================================
-# install Docker CE
-# ========================================================================
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 sudo apt-get update
 sudo apt-get install docker-ce -y
+
+
+# ========================================================================
+# install docker-compose
+# ========================================================================
+# https://docs.docker.com/compose/install/
+sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # ========================================================================
 # create docker usergroup and configure it
