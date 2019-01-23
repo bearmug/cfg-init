5b950940b9a87359375d4ecf3b3e12d3f733e759
bearmug <pavel.fadeev@gmail.com>
2019-01-24 00:10:10 +0300

    xclip installation

--- cfg-tools-visual.sh
+++ cfg-tools-visual.sh
@@ -8,4 +8,9 @@ sudo apt-get update
 sudo apt-get install fluxgui -y
 fluxgui &
 
+# ========================================================================
+# clipboard setup
+# ========================================================================
+sudo apt-get install xclip -y
+
 echo "### now your eyes going to feel better!"
