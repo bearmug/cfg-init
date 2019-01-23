e769b7fd330d628189a2f8b35dfd919aaa772027
Pavel Fadeev <pavel.fadeev@gmail.com>
2019-01-24 00:02:28 +0300

    Visual adjustments contributions (#3)
    * flux experience provided
    
    * change ohmyzsh theme
    
    * change git creds storage
    
    * update zsh theme
    
    * minortypo fixsture

--- cfg-tools-oh-my-zsh.sh
+++ cfg-tools-oh-my-zsh.sh
@@ -15,7 +15,7 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 # ========================================================================
 # zsh configure
 # ========================================================================
-sed -i "s/robbyrussell/avit/g" $HOME/.zshrc
+sed -i "s/robbyrussell/pygmalion/g" $HOME/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 sed -i "s/(git)/(git zsh-syntax-highlighting zsh-autosuggestions)/g" $HOME/.zshrc
---
+++ cfg-tools-visual.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ========================================================================
+# f.lux setup
+# ========================================================================
+sudo add-apt-repository ppa:nathan-renniewaldock/flux
+sudo apt-get update
+sudo apt-get install fluxgui -y
+fluxgui &
+
+echo "### now your eyes going to feel better!"
