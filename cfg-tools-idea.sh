bbee5c35a5890dce2602de5bab8f530352291508
Pavel Fadeev <pavel.fadeev@gmail.com>
2019-07-31 00:18:45 +0200

    ide to be instlled with toolbox (#10)

--- cfg-tools-idea.sh
+++ cfg-tools-idea.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 # ========================================================================
-# idea setup
-# ========================================================================
-sudo add-apt-repository ppa:mmk2410/intellij-idea-community -y
-sudo apt-get update
-sudo apt-get install intellij-idea-community -y
-sudo apt-get upgrade gradle -y
-
-# ========================================================================
 # proper fonts setup
 # ========================================================================
 curl -L https://github.com/hbin/top-programming-fonts/raw/master/install.sh | bash
-
 echo "Linux-like systems recommended fonts setup:"
 echo "Settings -> Editor -> Font -> Menlo"
 echo "Settings -> Appearance & Behavior -> Appearance -> Use custom font -> Noto Sans"
 
-echo "### IDEA installation passed OK"
+# ========================================================================
+# Intellij Toolbox page open
+# ========================================================================
+xdg-open https://www.jetbrains.com/toolbox/app/
+
+echo "### Intellij Toolbox page open OK"
+echo "### Remember to put jetbrains-toolbox binary to relevant path before use"
