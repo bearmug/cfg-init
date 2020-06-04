cf22243f6f1774172aea2b3c5940d9013c19a2de
Pavel Fadeev <pavel.fadeev@gmail.com>
2020-06-04 14:59:20 +0200

    make script universal across ubuntu versions

--- cfg-tools-docker.sh
+++ cfg-tools-docker.sh
@@ -8,7 +8,7 @@ sudo apt-get update
 # https://docs.docker.com/install/linux/docker-ce/ubuntu/
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -c) stable"
 sudo apt-get update
 sudo apt-get install docker-ce -y
 
