c6dc71e00249c082f4264297a97d88cb062a51e5
Pavel Fadeev <pavel.fadeev@gmail.com>
2020-06-04 15:06:28 +0200

    modernize docker setup

--- cfg-tools-docker.sh
+++ cfg-tools-docker.sh
@@ -6,11 +6,19 @@
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
 sudo apt-get update
 # https://docs.docker.com/install/linux/docker-ce/ubuntu/
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -c) stable"
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 sudo apt-get update
-sudo apt-get install docker-ce -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 
 # ========================================================================
