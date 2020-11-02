40ce786c346315da18b2eb7591404f9958b3213a
Pavel Fadeev <pavel.fadeev@gmail.com>
2020-11-02 22:01:07 +0100

    install pip3 and nvm tooling as well

--- cfg-tools-general-purpose.sh
+++ cfg-tools-general-purpose.sh
@@ -8,3 +8,9 @@ sudo apt-get install curl wget -y
 
 # database access clients
 sudo apt install mysql-client postgresql-client  -y
+
+# pip3 tooling
+sudo apt-get install python3-pip
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
