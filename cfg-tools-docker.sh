bef4c66743b4fc1335dd5fba91406ba3abba9457
Pavel Fadeev <pavel.fadeev@gmail.com>
2023-11-01 15:30:42 +0100

    Update cfg-tools-docker.sh
    bump docker-compose version

--- cfg-tools-docker.sh
+++ cfg-tools-docker.sh
@@ -25,7 +25,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 # install docker-compose
 # ========================================================================
 # https://docs.docker.com/compose/install/
-sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # ========================================================================
